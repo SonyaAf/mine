b33896aa3983b4445c4df79c7385c9b440693298
SonyaAf <61244947+SonyaAf@users.noreply.github.com>
2020-05-05 12:48:44 +0100

    Update 3_SFPDepartment_ANN.R

--- 3_SFPDepartment_ANN.R
+++ 3_SFPDepartment_ANN.R
@@ -15,36 +15,35 @@ library(dplyr)
 library(nnet)
 library("neuralnet")
 
-#Carregar os Datasets
+#Loading datasets
 DS_Treino <- read.csv("train.txt",stringsAsFactors = FALSE)   # DataSet Treino
 DS_Teste <- read.csv("teste.csv",stringsAsFactors = FALSE)   # DataSet teste
 
-#Acrescentar Novas Váriavies 
+#year
 Ano <- year(as.Date(DS_Treino$Dates))
 
-#Mês por extenso
+#month
 mes <- month(as.Date(DS_Treino$Dates), label=TRUE)
 
-#dia semana por extenso
+#day of week
 dia_semana=weekdays(as.Date(DS_Treino$Dates))
 
-#Dia
+#Day
 dia = as.Date(DS_Treino$Dates," %Y-%m-%d ")
 
-#Hora 
+#Hour
 hora = str_pad(hour(DS_Treino$Dates), 2, pad = "0") 
 
 
 #=============================ARTIFICIAL NEURAL NETWORK===============
-#Recorra à aplicação de ANN e faça uma comparação entre este e as restantes técnicas, 
-#em termos de precisão de predição.
+
 
 treinoANN <- mutate(DS_Treino, Ano, mes, dia_semana, dia, hora,X,Y)
 matrixFrametreino<- subset(treinoANN, select = c(Category,hora,dia_semana,PdDistrict,X,Y))
 matrixtreinocategory<-factor(matrixFrametreino[,1])
 matrixFrametreino[,1]<-as.numeric(matrixtreinocategory)
 matrixFrametreino[,2]<-as.numeric(hora)
-matrixFrametreino[,3] <-(sapply(matrixFrametreino[,3],switch,"domingo"=1,"segunda-feira"=2, "terça-feira"=3,"quarta-feira"=4,"quinta-feira"=5,"sexta-feira"=6,"sábado"=7))
+matrixFrametreino[,3] <-(sapply(matrixFrametreino[,3],switch,"domingo"=1,"segunda-feira"=2, "terÃ§a-feira"=3,"quarta-feira"=4,"quinta-feira"=5,"sexta-feira"=6,"sÃ¡bado"=7))
 matrixFrametreinodiasemana<-factor(matrixFrametreino[,3])
 matrixFrametreino[,3]<-as.numeric(matrixFrametreinodiasemana)
 matrixFrametreinodistrito<-factor(matrixFrametreino[,4])
@@ -55,42 +54,42 @@ View (matrixFrametreino)
 
 
 #ANN
-# funcao activação
+# activation function
 nn = neuralnet(Category~dia_semana+hora+PdDistrict+X+Y, data = matrixFrametreino, hidden = 2, err.fct = "sse", 
                linear.output = FALSE)   
 
-#modelo
+#model
 nn
 nn$result.matrix
 
-#a ANN inicial graficamente
+#a ANN graphic
 plot(nn)
 
-# os pesos iniciais da ANN
+# weights
 nn$weights
 
 
-#vars usadas para treino da ANN
+#vars for train
 nn$covariate
 
 nn1=ifelse(nn$net.result[[1]]>0.5,1,0)
 nn1
 
 
-# erro de classificação
+# classification errosr
 ClassificationError = mean(matrixFrametreino$Category !=nn1)  
 ClassificationError
 
-# percentagem de boa classificao
+# percentage of good classification
 PercentGoodClassification = (1 - ClassificationError)*100
 PercentGoodClassification
 
 
-#Iteracoes/observacao vamos ver se foi bem predito ou nao
+#Correct predictions
 OutputVsPred = cbind(matrixFrametreino$Category, nn1)
 OutputVsPred
 
-#Back Propagation para introdução do erro
+#Back Propagation 
 nn.BackProp = neuralnet(Category~dia_semana+hora+PdDistrict+X+Y, data = matrixFrametreino,hidden = 2, learningrate = 0.01,
                         algorithm = "backprop", err.fct="sse",  
                         linear.output = FALSE)
@@ -101,19 +100,19 @@ plot(nn.BackProp)
 
 
 # Confusion Matrix & Misclassification Error - testing data
-#Acrescentar Novas Váriavies 
+#Acrescentar Novas VÃ¡riavies 
 Ano2 <- year(as.Date(DS_Teste$Dates))
 
-#Mês por extenso
+#Month
 mes2 <- month(as.Date(DS_Teste$Dates), label=TRUE)
 
-#Mês por extenso
+#day of week
 dia_semana2=weekdays(as.Date(DS_Teste$Dates))
 
-#Dia
+#Day
 dia2 = as.Date(DS_Teste$Dates," %Y-%m-%d ")
 
-#Hora 
+#Hour 
 hora2 = str_pad(hour(DS_Teste$Dates), 2, pad = "0")
 
 #distrito
@@ -125,12 +124,11 @@ x2 = as.numeric(DS_Teste$X)
 #Y
 y2 = as.numeric(DS_Teste$Y)
 
-#Acrescentar novas váriáveis ao DataSet
-
+#data test variables
 teste <- mutate(DS_Teste, Ano2, mes2, dia_semana2, dia2, hora2, distrito2,x2,y2)
 #matrixFrame2<- subset(teste, select = c(hora,dia_semana,distrito))
 matrixteste<- subset(teste, select = c(dia_semana2, dia2, hora2, distrito2,x2,y2))
-matrixteste[,1] <-(sapply(matrixteste[,1],switch,"domingo"=1,"segunda-feira"=2, "terça-feira"=3,"quarta-feira"=4,"quinta-feira"=5,"sexta-feira"=6,"sábado"=7))
+matrixteste[,1] <-(sapply(matrixteste[,1],switch,"domingo"=1,"segunda-feira"=2, "terÃ§a-feira"=3,"quarta-feira"=4,"quinta-feira"=5,"sexta-feira"=6,"sÃ¡bado"=7))
 matrixtestediasemana<-factor(matrixteste[,1])
 matrixteste[,1]<-as.numeric(matrixtestediasemana)
 matrixteste[,2]<-as.numeric(dia2)
@@ -140,22 +138,22 @@ matrixteste[,5]<-as.numeric(teste$x2)
 matrixteste[,6]<-as.numeric(teste$Y)
 View (matrixteste)
 
-#================PREDICAO==============================
+#================PREDICTION==============================
 nn.output = compute(nn, covariate=matrix(c(matrixteste$dia_semana2,matrixteste$hora2,matrixteste$distrito2,matrixteste$y2,matrixteste$x2),byrow = TRUE,ncol=5))
 
-#verificar correspondencia
+#check correct prediction
 #check<-subset(teste, select = c(PdDistrict,distrito2))
 #View(check)
-#treino
+#train
 nn.output1 = compute(nn, covariate=matrix(c(matrixFrametreino$dia_semana,matrixFrametreino$hora,matrixFrametreino$PdDistrict,matrixFrametreino$Y,matrixFrametreino$X),byrow = TRUE,ncol=5))
 
-#probabilidade de predicao/probabilidade do acontecimento
+#probability of prediction / probability of the event
 nn.output$net.result
 nn.output1$net.result
 
 par(mfrow=c(2,2))
 #min = -2.5, max = 5
-#existe variabilidade?
+#variability?
 gwplot(nn, selected.covariate = "dia_semana")
 gwplot(nn, selected.covariate = "hora")
 gwplot(nn, selected.covariate = "PdDistrict")
@@ -164,7 +162,7 @@ gwplot(nn, selected.covariate = "Y")
 #Error in plot.new() : figure margins too large
 
 
-# Alterar numero hidden layers 4
+# hidden layers 4
 nn.BackProp2 = neuralnet(Category~dia_semana+hora+PdDistrict+X+Y, data = matrixFrametreino, hidden = c(4,3,2,1) , learningrate = 0.01,act.fct="tanh",
                          algorithm = "backprop", err.fct="sse", linear.output = FALSE)
 
@@ -176,11 +174,11 @@ nn
 
 nn2=ifelse(nn$net.result[[1]]>0.5,1,0)
 nn2
-# erro de classificação:
+# classification error:
 ClassificationError2 = mean(matrixFrametreino$Category !=nn2)  
 ClassificationError2
 
-#Percentagem de boa classificação 
+#Percentage of good classification
 PercentGoodClassification2 = (1 - ClassificationError2)*100
 PercentGoodClassification2
 
@@ -190,7 +188,7 @@ OutputVsPred2 = cbind(matrixFrametreino$Category, nn2)
 OutputVsPred2
 
 #==========================================================================
-# COM 6 HIDDEN
+# hidden layers 6
 nn.BackProp3 = neuralnet(Category~dia_semana+hora+PdDistrict+X+Y, data = matrixFrametreino, hidden = c(6,5,4,3,2,1) , learningrate = 0.01,act.fct="tanh",
                          algorithm = "backprop", err.fct="sse", linear.output = FALSE)
 
@@ -203,11 +201,11 @@ plot(nn.BackProp3)
 nn3=ifelse(nn$net.result[[1]]>0.5,1,0)
 nn3
 
-### erro de classificação
+### classification error:
 ClassificationError3 = mean(matrixFrametreino$Category !=nn3)  
 ClassificationError3
 
-#percentagem de boa classificação
+#Percentage of good classification
 PercentGoodClassification3 = (1 - ClassificationError3)*100
 PercentGoodClassification3
 
